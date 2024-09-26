7a65d528f5d0b06ba318d97b2895e00ae75157dc
sergio-franca-wave <139240473+sergio-franca-wave@users.noreply.github.com>
2024-09-26 10:46:53 -0300

    .

--- apps/local/reviews-app.ts
+++ apps/local/reviews-app.ts
@@ -1,2 +1,2 @@
-export { default } from "apps/reviews-app/mod.ts";
-export * from "apps/reviews-app/mod.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/betovawn1/reviews@v1.0.0/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/betovawn1/reviews@v1.0.0/mod.ts";
